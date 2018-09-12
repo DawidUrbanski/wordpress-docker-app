e2275604d9be1633870ffd8a8d79017ee3f9254d
Dawid Urbański <hi@dawidurbanski.com>
2018-09-12 12:45:06 +0200

    New app

--- config/wp.sh
+++ config/wp.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 # done just once
-if ! $(wp core is-installed); then
+if ! $(wp core is-installed --allow-root); then
 
 	# install wordpress
-	wp core install --url='{placeholder}.localhost' --title='{placeholder}' --admin_user=admin --admin_password=admin --admin_email='admin@{placeholder}.localhost' --skip-email
+	wp core install --url='{placeholder}.localhost' --title='{placeholder}' --admin_user=admin --admin_password=admin --admin_email='admin@{placeholder}.localhost' --skip-email --allow-root
 
 	# clean up
-	wp rewrite structure '/%postname%/' --hard
-	wp plugin uninstall hello --deactivate
-	wp plugin uninstall akismet --deactivate
+	wp rewrite structure '/%postname%/' --hard --allow-root
 
 fi
 
-# dependencies
-wp plugin install 'https://github.com/Kubitomakita/mailhog-wp-smtp/archive/master.zip' --activate
+if ! $(wp plugin is-installed 'https://github.com/Kubitomakita/mailhog-wp-smtp/archive/master.zip'  --allow-root); then
+
+	# dependencies
+	wp plugin install 'https://github.com/Kubitomakita/mailhog-wp-smtp/archive/master.zip'  --activate --allow-root
+
+fi
